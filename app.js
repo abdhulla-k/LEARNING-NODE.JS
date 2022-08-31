0a7451389102385a5574c76a4f2b40fce09eaf0b
abdhulla-k <abdhullak149@gmail.com>
2022-08-31 17:09:38 +0530

    learned about middlewares in express and send() method to send response

--- app.js
+++ app.js
@@ -4,6 +4,24 @@ const http = require( 'http' ); // imported http module or object
 // import aur callback function from routes.js file
 // const routes = require( './routes' );
 
+// import express.js
+const express = require( "express" );
+
+// get express as a function
+const app = express();
+
+// it will only run top to bottom. you have to use next() to jump to next middle ware
+app.use(( req, res, next ) => {
+    console.log( "first middle ware" );
+    // you have to use next() method to jump to next middle ware like bellow
+    next(); // allow us to request to continue to the next middleware in line
+});
+
+app.use(( req, res, next ) => {
+    
+    // use res.send() method to send a response
+    res.send( "<h1>Hello From Express!</h1>" );
+});
 
 
 // http module has a method called 'createServer()'. It is using to create a server like below. 
@@ -13,7 +31,7 @@ const http = require( 'http' ); // imported http module or object
 // we can use process.exit() to end the event loop. but we dont want to do that.
 
 // eg.
-const server = http.createServer();
+const server = http.createServer( app );
 
 // to start litening
 server.listen( 3000 );
