b20886be18788d8f572176e9558f51436fc57581
abdhulla-k <abdhullak149@gmail.com>
2022-08-31 17:14:53 +0530

    learned listen() function

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // the function 'require()' using to import a module or something like below
-const http = require( 'http' ); // imported http module or object
+// const http = require( 'http' ); // imported http module or object
 
 // import aur callback function from routes.js file
 // const routes = require( './routes' );
@@ -18,7 +18,7 @@ app.use(( req, res, next ) => {
 });
 
 app.use(( req, res, next ) => {
-    
+
     // use res.send() method to send a response
     res.send( "<h1>Hello From Express!</h1>" );
 });
@@ -30,8 +30,11 @@ app.use(( req, res, next ) => {
 // You have to save the server created and call 'listen(<port number>)' function to listen the server.
 // we can use process.exit() to end the event loop. but we dont want to do that.
 
-// eg.
-const server = http.createServer( app );
+// // eg.
+// const server = http.createServer( app );
 
-// to start litening
-server.listen( 3000 );
+// // to start litening
+// server.listen( 3000 );
+
+// use app.listen(<posrt number>) function to able to listen from a browser. insted of writing mannually
+app.listen( 3000 );
