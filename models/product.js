842fec9366c50a80291a8417782e0e46f3c3084f
abdhulla-k <abdhullak149@gmail.com>
2022-09-15 20:24:07 +0530

    used fs module to save data. implemented mvc

--- models/product.js
+++ models/product.js
@@ -1,4 +1,22 @@
-const products = [];
+const fs = require( 'fs' );
+const path = require( 'path' );
+
+const p = path.join( 
+    path.dirname( require.main.filename ), 
+    'data', 
+    'products.json' 
+);
+
+
+const getProductsFormFile = ( cb ) => {
+    fs.readFile( p, ( err, fileContent ) => {
+        if( err ) {
+            return cb([]);
+        } else {
+            cb( JSON.parse( fileContent ));
+        };
+    });
+}
 
 module.exports = class Product {
     constructor( t ) {
@@ -6,10 +24,15 @@ module.exports = class Product {
     }
 
     save() {
-        products.push( this );
+        getProductsFormFile( products => {
+            products.push( this );
+            fs.writeFile( p, JSON.stringify( products ), (err) => {
+                console.log( err );
+            });
+        });
     }
 
-    static fetchAll() {
-        return products;
+    static fetchAll( cb ) {
+        getProductsFormFile( cb );
     }
-}
+};
