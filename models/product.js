0e78a816247c54d9d69f41213cd305d7bcbf1247
abdhulla-k <abdhullak149@gmail.com>
2022-09-16 10:17:09 +0530

    restructured project in mvc patter and also for a shoppint cart. added more ejs files and js files

--- models/product.js
+++ models/product.js
@@ -1,38 +1,46 @@
-const fs = require( 'fs' );
-const path = require( 'path' );
+// import modules
+const fs = require('fs');
+const path = require('path');
 
-const p = path.join( 
-    path.dirname( require.main.filename ), 
-    'data', 
-    'products.json' 
+// path set
+const p = path.join(
+  path.dirname(process.mainModule.filename),
+  'data',
+  'products.json'
 );
 
+// helper function to get products
+const getProductsFromFile = cb => {
+  fs.readFile(p, (err, fileContent) => {
+    if (err) {
+      cb([]);
+    } else {
+      cb(JSON.parse(fileContent));
+    }
+  });
+};
 
-const getProductsFormFile = ( cb ) => {
-    fs.readFile( p, ( err, fileContent ) => {
-        if( err ) {
-            return cb([]);
-        } else {
-            cb( JSON.parse( fileContent ));
-        };
-    });
-}
-
+//  model calss
 module.exports = class Product {
-    constructor( t ) {
-        this.title = t;
-    }
+  constructor( title, imageUrl, description, price ) {
+    this.title = title;
+    this.imageUrl = imageUrl;
+    this.description = description = description;
+    this.price = price;
+  }
 
-    save() {
-        getProductsFormFile( products => {
-            products.push( this );
-            fs.writeFile( p, JSON.stringify( products ), (err) => {
-                console.log( err );
-            });
-        });
-    }
+  // save function to save new data
+  save() {
+    getProductsFromFile(products => {
+      products.push(this);
+      fs.writeFile(p, JSON.stringify(products), err => {
+        console.log(err);
+      });
+    });
+  }
 
-    static fetchAll( cb ) {
-        getProductsFormFile( cb );
-    }
+  // fetching function to fetch all data
+  static fetchAll(cb) {
+    getProductsFromFile(cb);
+  }
 };
