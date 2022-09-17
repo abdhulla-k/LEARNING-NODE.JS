4e74c5c5d0f01b2e4447e59afce78f8ab4e63d79
abdhulla-k <abdhullak149@gmail.com>
2022-09-17 21:36:09 +0530

    added edit product functionality

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,5 +1,5 @@
 const Product = require('../models/product');
-const Cart = require( '../models/cart' );
+const Cart = require('../models/cart');
 
 exports.getProducts = (req, res, next) => {
   Product.fetchAll(products => {
@@ -14,8 +14,8 @@ exports.getProducts = (req, res, next) => {
 exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId;
   Product.findById(prodId, product => {
-    res.render( 'shop/product-detail', { 
-      product: product, 
+    res.render('shop/product-detail', {
+      product: product,
       pageTitle: product.title,
       path: '/products'
     });
@@ -39,12 +39,12 @@ exports.getCart = (req, res, next) => {
   });
 };
 
-exports.postCart = ( req, res, next ) => {
+exports.postCart = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.findById( prodId, (product) => {
-    Cart.addProduct( prodId, product.price );
-  })
-  res.redirect( '/cart' );
+  Product.findById(prodId, product => {
+    Cart.addProduct(prodId, product.price);
+  });
+  res.redirect('/cart');
 };
 
 exports.getOrders = (req, res, next) => {
