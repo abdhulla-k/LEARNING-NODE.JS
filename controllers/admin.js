403e849beec511ac5182b63ec8204f89cce7e719
abdhulla-k <abdhullak149@gmail.com>
2022-09-18 16:28:34 +0530

    created update product functionality with mongoose

--- controllers/admin.js
+++ controllers/admin.js
@@ -37,17 +37,17 @@ exports.getEditProduct = (req, res, next) => {
     return res.redirect('/');
   }
   const prodId = req.params.productId;
-  Product.findById(prodId, product => {
-    if (!product) {
-      return res.redirect('/');
-    }
-    res.render('admin/edit-product', {
+
+  Product.findById( prodId ).then( product => {
+    res.render( 'admin/edit-product', {
       pageTitle: 'Edit Product',
       path: '/admin/edit-product',
       editing: editMode,
       product: product
-    });
-  });
+    })
+  }).catch( err => {
+    res.redirect('/');
+  })
 };
 
 exports.postEditProduct = (req, res, next) => {
@@ -56,15 +56,22 @@ exports.postEditProduct = (req, res, next) => {
   const updatedPrice = req.body.price;
   const updatedImageUrl = req.body.imageUrl;
   const updatedDesc = req.body.description;
-  const updatedProduct = new Product(
-    prodId,
-    updatedTitle,
-    updatedImageUrl,
-    updatedDesc,
-    updatedPrice
-  );
-  updatedProduct.save();
-  res.redirect('/admin/products');
+  
+  Product.findById( prodId )
+    .then( product => {
+      product.title = updatedTitle;
+      product.price = updatedPrice;
+      product.description = updatedDesc;
+      product.imageUrl = updatedImageUrl;
+      return product.save();
+    })
+    .then( result => {
+      console.log( 'updated product' );
+      res.redirect( '/admin/products' );
+    })
+    .catch( err => {
+      console.log( err );
+    })
 };
 
 exports.getProducts = (req, res, next) => {
