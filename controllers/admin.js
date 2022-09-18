c549abaa87267c9b95b9333a7106d1d16d898788
abdhulla-k <abdhullak149@gmail.com>
2022-09-18 15:49:31 +0530

    created fetch controller from admin products

--- controllers/admin.js
+++ controllers/admin.js
@@ -68,13 +68,17 @@ exports.postEditProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll(products => {
-    res.render('admin/products', {
-      prods: products,
-      pageTitle: 'Admin Products',
-      path: '/admin/products'
-    });
-  });
+  Product.find()
+    .then( products => {
+      res.render('admin/products', {
+        prods: products,
+        pageTitle: 'Admin Products',
+        path: '/admin/products'
+      });
+    })
+    .catch( err => {
+      console.log( err );
+    })
 };
 
 exports.postDeleteProduct = (req, res, next) => {
